f4981a63e3d9e776d130207392f752ce327190f7
Nathan Chen <chenl.nathan@gmail.com>
2017-11-16 23:20:50 -0800

    test.js has a function using jqueryUI that autocompletes the city being searched for

--- test.js
+++ test.js
@@ -1,21 +1,30 @@
+//Autocomplete request/response expectations: https://stackoverflow.com/questions/5077409/what-does-autocomplete-request-server-response-look-like
+//Requires  ajax/jquery UI and style
 $(function () 
  {
 	 $("#f_elem_city").autocomplete({
 		source: function (request, response) {
 		 $.getJSON(
-			"http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
+		 	// "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=" + request.term,
+			"http://free.rome2rio.com/api/1.4/json/Autocomplete?key=7slkssQD&query="+request.term,
 			function (data) {
-			 response(data);
-			 console.log(data);
-			 console.log(request);
+			var placesArray = [];
+			 for(let i = 0; i<data.places.length; i++){
+			 	var valueObject = {
+			 		"value": data.places[i].longName
+			 	}
+			 	placesArray.push(valueObject);
+			 }
+			 response(placesArray);
 			}
 		 );
 		},
 		minLength: 3,
 		select: function (event, ui) {
 		 var selectedObj = ui.item;
+		 console.log(selectedObj);
 		 $("#f_elem_city").val(selectedObj.value);
-		getcitydetails(selectedObj.value);
+	
 		 return false;
 		},
 		open: function () {
@@ -30,16 +39,16 @@ $(function ()
 
 });
 
-$.getJSON(
-     "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=los",
-    // After the data comes back from the API
-    function(response) {
-      // Storing an array of results in the results variable
+// $.getJSON(
+//      "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=los",
+//     // After the data comes back from the API
+//     function(response) {
+//       // Storing an array of results in the results variable
      	
-     	console.log(response);
+//      	console.log(response);
  
 
-    });
+//     });
 
 
 
