fcd198017d66846733e740951c9c28a292f6b1fd
orpillasam <31043722+orpillasam@users.noreply.github.com>
2017-11-30 10:58:05 -0800

    added modal how-to, changed bottom text

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -275,6 +275,34 @@ $(document).ready(function() {
 		$(this).closest('.card-div').remove();
 	});
 
+
+
+		// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("modal-button");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}	
+
 });
 	//script to add animations to elements that are clicked with animiate.css classes added to it
 	$.fn.extend({
@@ -288,7 +316,11 @@ $(document).ready(function() {
 	        });
 	        return this;
 	    }
+
+
 });
+
+
 
 
 /*---------------------------------------Travel Card Functions----------------------------------- 
